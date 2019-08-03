46ae9c066acb35848c6e57c9b0453a75461afc64
Michael Schroder <thormme@yahoo.com>
2019-08-03 03:09:29 -0700

    Actually render the things

--- src/client/component/App.tsx
+++ src/client/component/App.tsx
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="component-app">
-        hi
+        Welcome to the wonderous world of <i>Strife</i>
       </div>
     );
   }
--- src/server/index.ts
+++ src/server/index.ts
@@ -5,5 +5,6 @@ const app = Express();
 const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
+app.get('/bundle.js', (req, res) => res.sendFile(path.resolve(__dirname, 'bundle.js')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
