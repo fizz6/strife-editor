99839a7c74dd86b96c8768a27ae47d45fb6cfd1f
Michael Schroder <thormme@yahoo.com>
2019-08-17 18:39:19 -0700

    Can now move player with arrow keys

--- src/client/component/App.tsx
+++ src/client/component/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Optional } from '@fizz6/strife/src/Optional';
 import { Dispatcher } from '@fizz6/strife-common';
-import { Scene } from '@fizz6/strife-common';
+import { Scene, Entity } from '@fizz6/strife-common';
 import { UpdateEvent } from '../events/UpdateEvent';
 import { RenderEvent } from '../events/RenderEvent';
 import { Player } from './Player';
+import { Input } from './Input';
 import "./App.css";
 
 export default class App extends React.Component {
@@ -11,7 +13,22 @@ export default class App extends React.Component {
   ctx: CanvasRenderingContext2D | null = null;
   scene: Scene = new Scene();
 
-   mainLoop = () => {
+  static rootEntity: Entity | null = null;
+  static get root(): Entity {
+      if (App.rootEntity) {
+          return App.rootEntity;
+      }
+      throw "Initialization not finished";
+  }
+  static get input(): Input {
+      let input: Optional<Input> = App.root.components.get(Input);
+      if (input) {
+          return input;
+      }
+      throw "Initialization not finished";
+  }
+
+  mainLoop = () => {
     window.requestAnimationFrame(this.mainLoop);
 
     this.scene.dispatcher.emit(new UpdateEvent(.05));
@@ -24,8 +41,12 @@ export default class App extends React.Component {
 
   componentDidMount = () => {
       this.scene.components.register(Player);
+      this.scene.components.register(Input);
       let playerEntity = this.scene.entities.add();
       let player = playerEntity.components.add(Player);
+
+      App.rootEntity = this.scene.entities.add();
+      let input = App.rootEntity.components.add(Input);
 
       this.mainLoop();
   }
--- src/client/component/Player.ts
+++ src/client/component/Player.ts
@@ -1,25 +1,45 @@
 import { Component, Entity } from '@fizz6/strife-common';
+import { Optional } from '@fizz6/strife/src/Optional';
 import Vector2 from '@fizz6/strife-math/src/Vector2';
 import { UpdateEvent } from '../events/UpdateEvent';
 import { RenderEvent } from '../events/RenderEvent';
+import Serialization from '@fizz6/serialization';
+import App from './App';
+import { Input } from './Input';
 
+@Serialization.type
 export class Player extends Component {
     position: Vector2;
+    speed: Vector2;
 
     constructor(entity: Entity) {
         super(entity);
         this.position = Vector2.Zero;
+        this.speed = Vector2.Zero;
     }
 
     @Component.On(UpdateEvent)
     update(event: UpdateEvent): void {
-        this.position.x+=.05;
+        this.speed = Vector2.Zero;
+        if (App.input.pressedKeys.get('ArrowLeft')) {
+            this.speed.add(new Vector2(-1, 0));
+        }
+        if (App.input.pressedKeys.get('ArrowRight')) {
+            this.speed.add(new Vector2(1, 0));
+        }
+        if (App.input.pressedKeys.get('ArrowUp')) {
+            this.speed.add(new Vector2(0, -1));
+        }
+        if (App.input.pressedKeys.get('ArrowDown')) {
+            this.speed.add(new Vector2(0, 1));
+        }
+        this.position.add(this.speed);
     }
 
     @Component.On(RenderEvent)
     render(event: RenderEvent) {
         event.context.clearRect(0, 0, event.canvas.width, event.canvas.height);
         event.context.fillStyle = 'green';
-        event.context.fillRect(this.position.x, 10, 150, 100);
+        event.context.fillRect(this.position.x, this.position.y, 50, 50);
     }
 }
